ea1890947fe18c6822fbdd3b079470729c4f7370
Johannes Degn <j@degn.de>
2014-07-26 12:35:08 +0200

    update export list

--- src/FactorModels.jl
+++ src/FactorModels.jl
@@ -5,15 +5,15 @@ using DataFrames
 
 import GLM.predict
 
-export FactorModel, DynamicFactorModel, predict, calculate_factors,
-    lag_vector, factor_model_DGP, normalize, pseudo_out_of_sample_forecasts, MSE, benchmark_forecasts,
+export FactorModel, DynamicFactorModel, predict, calculate_factors, scree_plot,
+    lag_vector, lagged_matrix, factor_model_DGP, normalize, pseudo_out_of_sample_forecasts, MSE, RMSE, benchmark_forecasts, benchmark_ar, diebold_mariano, matrix_to_table, print_matrix,
     targeted_predictors,
     wild_bootstrap, residual_bootstrap,
-    LR_test, LM_test, Wald_test
+    LR_test, LM_test, LM_test_gls, Wald_test, quandt_andrews, quandt_andrews_critical_value, chow_test
 
-include("utils.jl")
 include("FactorModel.jl")
 include("DynamicFactorModel.jl")
+include("utils.jl")
 include("targeted_predictors.jl")
 include("bootstrap.jl")
 include("chowtest.jl")
