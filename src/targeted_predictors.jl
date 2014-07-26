b552b4ace2401bda64d6084f8c3eb90b82fd1c6f
Johannes Degn <j@degn.de>
2014-07-26 12:36:17 +0200

    fix targeted predictors, soft and hard thresholding

--- src/targeted_predictors.jl
+++ src/targeted_predictors.jl
@@ -1,38 +1,36 @@
-function targeted_predictors(y::Array{Float64,1}, w::Matrix{Float64}, x::Matrix{Float64}; thresholding::String="hard")
-    # Bai and Ng (2008) -> regress y on w and x and keep only the predictors x which are significant
-    # this functions returns their column numbers
-    # note that w should maybe consist of a constant (vector of 1s) and lags of y
-    #predictors = DataFrame(hcat(y, w, x))  # unfortunately using formulae and the GLM package is a bit clumsy here
-    #lm(Formula(convert(Symbol, "x1"), convert(Symbol, join(names(predictors[2:end]), "+"))), predictors)
-    #lm(*("x1~", join(names(predictors[2:end]), "+")), predictors)
+using LARS
 
-    design_matrix = hcat(w, x)
-    if thresholding == "hard"  # TODO: hard thresholding does not work properly, only admits one variable plus the white covariance has negative diagonal elements
-        coefficients = inv(design_matrix'*design_matrix)*(design_matrix'*y)  # OLS estimate
-        residuals = y - design_matrix * coefficients
-        # calcualte variance-covariance matrix according to White(1980) TODO: argument to choose if robust or not
-        coefficient_covariance = inv(design_matrix'design_matrix)*(design_matrix'*diagm(residuals.^2)*design_matrix)*inv(design_matrix'design_matrix)
-        num_negative = sum(diag(coefficient_covariance) .< 0)
-        if num_negative > 0
-            for i in 1:size(coefficient_covariance, 1)
-                if coefficient_covariance[i, i] < 0
-                   coefficient_covariance[i, i] = -coefficient_covariance[i, i]
-               end
-            end
-            t_stat = coefficients./sqrt(diag(coefficient_covariance))
-            println("there were $num_negative negative coefficient variances which have been set positive. THIS IS UNACCEPTABLE!")
-        else
-            t_stat = coefficients./sqrt(diag(coefficient_covariance))
-        end
-        critical_value = quantile(TDist(length(y)-length(coefficients)), 0.975)
-        println(sum(abs(t_stat[size(w, 2)+1:end]) .> critical_value))
-        return abs(t_stat[size(w, 2)+1:end]) .> critical_value  # return significant t-stats associated with x variables only (Bai, Ng, 2008)
-    end
-    if thresholding == "soft"  # lasso coefficients which are non 0 are used
-        #res=lars(design_matrix, y, intercept=true, standardize=true, use_gram=true, maxiter=typemax(Int), lambda_min=0.0, verbose=true)
-        res = GLMNet.glmnetcv(design_matrix, y)  # TODO: GLMNet should not be necessary here...
-        betas = res.path.betas[:, indmin(res.meanloss)]
-        return abs(betas[size(w, 2)+1:end]) .> 0
-    end
+function get_t_stat(y, w, x_i)
+    design_matrix = hcat(ones(size(w, 1)), w[:, 2:end], x_i[size(w, 2):end])
+    coefficients = inv(design_matrix'*design_matrix)*(design_matrix'*y)  # OLS estimate
+    residuals = y - design_matrix * coefficients
+    # calcualte variance-covariance matrix according to White(1980)
+    coefficient_covariance = inv(design_matrix'design_matrix)*(design_matrix'*diagm(residuals.^2)*design_matrix)*inv(design_matrix'design_matrix)
+    t_stat = coefficients./sqrt(diag(coefficient_covariance))
+    last(t_stat)
 end
 
+function targeted_predictors(y_index::Int64, x::Matrix{Float64}, number_of_lags::Int64, thresholding::String="hard"; number_of_steps_in_lars=30, significance_level=0.05)
+    # Bai and Ng (2008) -> regress y on w and each x_i and keep only the predictors x which are significant
+    #predictors = DataFrame(hcat(y, w, x))  # unfortunately using formulae and the GLM package is a bit clumsy here
+    w = lagged_matrix(x[:, y_index], [0:number_of_lags])
+    y = w[:, y_index]
+    if thresholding == "hard"  # TODO: hard thresholding does not work properly, only admits one variable plus the white covariance has negative diagonal elements
+        critical_t_stat = quantile(TDist(size(w, 1) - (size(w, 2) + 1)), 1-significance_level/2)  # number of parameters is constant + lag number + x_i
+        t_stats = Float64[get_t_stat(y, w, x[:, i]) for i in [1:size(x, 2)]]  # t_stat for y regressed on itself will be huge!
+        return abs(t_stats) .> critical_t_stat
+    end
+    if thresholding == "soft"  # lasso coefficients which are non 0 are used
+        design_matrix = hcat(x[size(w, 2):end, [1:size(x,2)] .!= y_index], w[:, 2:end])
+        #res=lars(design_matrix, y, intercept=true, standardize=true, use_gram=true, maxiter=typemax(Int), lambda_min=0.0, verbose=true)
+        res = lars(design_matrix, y)  # adds an intercept by default
+        in_the_model = falses(size(x, 2))
+        betas_added = Int[step.added for step in res.steps]
+        betas_added = betas_added[betas_added.<size(x, 2)]  # only set the values for beta coefficients to true (not for coefficients of w)
+        betas_added = betas_added[betas_added.>0][1:number_of_steps_in_lars]  # in some steps no variable is added
+        betas_added[betas_added.>y_index] = betas_added[betas_added.>y_index] + 1  # all indices after y_index need to be shifted by 1 to account for y
+        in_the_model[betas_added] = true
+        in_the_model[y_index] = true
+        return in_the_model
+    end
+end
